cb930e6b81e717ed6ccc9f7070815471dafa71e2
Anurag Rai <aadamgamer07@gmail.com>
2026-05-01 13:20:50 +0530

    Added admin get-alerts endpoint

--- src/main/java/com/resilientcampus/disaster/disaster/SOSController.java
+++ src/main/java/com/resilientcampus/disaster/disaster/SOSController.java
@@ -2,7 +2,9 @@ package com.resilientcampus.disaster.disaster;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Date;
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(origins = "*")
@@ -18,5 +20,9 @@ public class SOSController {
         System.out.println("🚨 CLOUD SOS RECEIVED from: " + request.getStudentId());
 
         return "SOS Signal Received and Saved to Cloud!";
+    }
+    @GetMapping("/all-alerts")
+    public List<SOSRequest> getAllAlerts() {
+        return sosRepository.findAll();
     }
 }
